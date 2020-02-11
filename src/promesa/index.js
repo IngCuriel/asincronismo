4d05ab3ea7ca568800e92575e37dfd6ac5bbe594
Eleazar Curiel Monjaraz <eleazar-00@hotmail.com>
2020-02-11 12:56:24 -0600

    Se resolvio promesas encadenadass

--- src/promesa/index.js
+++ src/promesa/index.js
@@ -10,6 +10,34 @@ const somethingWillHappen = () => {
   });
 };
 
+const somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      // se setTimeout resive dos argumentos
+      //1--resive una funcion
+      //2-- en tiempo en que se va ejecutar en milisegundos
+      setTimeout(() => {
+        resolve("True");
+      }, 2000);
+    } else {
+      const error = new Error("whooops!");
+      reject(error);
+    }
+  });
+};
+
 somethingWillHappen()
   .then(response => console.log(response))
   .catch(error => console.log(error));
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(error => console.error(error));
+
+//Como Correr varias promesas(Promesas encadenadas) y retornar un arreglo con los resultado
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => {
+    console.log("Array of result", response);
+  })
+  .catch(error => console.error(error));
